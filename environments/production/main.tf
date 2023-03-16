079fb3b795fd28412105f4fa9d55d113157bf5e5
Gabriel Hodoroaga <gabihodoroaga@gmail.com>
2023-03-17 00:24:31 +0200

    Fix production

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -8,3 +8,11 @@ locals {
 provider "google" {
   project = local.project
 }
+
+module "pubsub_to_bq" {
+  source                = "../../modules/pubsub-to-bq"
+  project               = local.project
+  region                = local.region
+  env                   = local.env
+  branch                = local.branch
+}
